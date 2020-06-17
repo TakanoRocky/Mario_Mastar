728736b185208bac29aa78f0b27bfaf29761b905
TakanoRocky <soldierrocky0702@gmail.com>
2020-06-18 00:22:27 +0900

    プロジェクト作成

---
+++ App.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "DxLib.h"
+
+void Init();
+
+void Update();
+
+void Draw();
---
+++ move.cpp
@@ -0,0 +1,30 @@
+#include "DxLib.h"
+#include "App.h"
+
+// プログラムは WinMain から始まります
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+
+	ChangeWindowMode(TRUE);    //
+	SetGraphMode(1024, 896, 32);
+
+	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
+	{
+		return -1;			// エラーが起きたら直ちに終了
+	}
+	SetDrawScreen(DX_SCREEN_BACK);	//裏画面を描画対象へ
+
+	Init();
+
+	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
+	{
+
+		Update();
+
+		Draw();
+	}
+
+	DxLib_End();				// ＤＸライブラリ使用の終了処理
+
+	return 0;				// ソフトの終了 
+}
